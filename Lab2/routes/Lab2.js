2cab2228c3ee28de6fb1d09553c438382ca7df22
Akshay Bansal <43522054+200395887@users.noreply.github.com>
2019-09-25 23:43:05 -0400

    Lab 2

---
+++ Lab2/routes/Lab2.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    //res.render('index', { title: 'Express' });
+
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y);
+
+    if (req.query.method == 'add' && req.query.x && req.query.y) {
+        var sum = x + y;
+        res.send('Adding ' + req.query.x + ' + ' + req.query.y + ' = ' + sum);
+    }
+    else if (req.query.method == 'subtract' && req.query.x && req.query.y) {
+        var sub = x - y;
+        res.send('Subtracting ' + req.query.x + ' - ' + req.query.y + ' = ' + sub);
+    }
+    else if (req.query.method == 'divide' && req.query.x && req.query.y) {
+        var division = x / y;
+        res.send('Dividing ' + req.query.x + ' / ' + req.query.y + ' = ' + division);
+    }
+    else if (req.query.method == 'multiply' && req.query.x && req.query.y) {
+        var muli = x * y;
+        res.send('Multiplying ' + req.query.x + ' * ' + req.query.y + ' = ' + multi);
+    }
+    else
+        res.send('Basic Calculations: To do the calculations type "lab2?method=(methodName)&x=#&y=#"');
+       });
+
+module.exports = router;
